eb628018c47707d9cf373becea1717cc92036ee6
kpi-developpement <kpi-developpement>
2026-06-03 15:23:59 +0100

    Add Omni Search with update bugs

--- Workflow/src/main/java/com/kyntus/Workflow/controller/PilotRecordController.java
+++ Workflow/src/main/java/com/kyntus/Workflow/controller/PilotRecordController.java
@@ -12,6 +12,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
